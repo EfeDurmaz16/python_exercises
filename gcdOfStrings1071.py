30e86c91ed7c899fd6589e7cafbf00d018b0c505
efebarandurmaz05@gmail.com <efebarandurmaz05@gmail.com>
2025-01-30 20:58:34 +0300

    Refactor gcdOfStrings method to improve logic and add edge case handling with test cases

--- gcdOfStrings1071.py
+++ gcdOfStrings1071.py
@@ -1,14 +1,23 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        len1, len2 = len(str1), len(str2)
+        if str1 + str2 != str2 + str1:
+            return ""
+        
+        if not str1:
+            return str2
+        if not str2:
+            return str1
 
-        def isDivisor(l) -> bool:
-            if(len1 % l != 0 or len2 % l != 0):
-                return False
-            f1, f2 = len1 // l, len2 // l
-            return str1[:l] * f1 == str1 and str1[:l] * f2 == str2
+        if len(str1) > len(str2):
+            return self.gcdOfStrings(str1[len(str2):], str2)
+        elif len(str1) < len(str2):
+            return self.gcdOfStrings(str1, str2[len(str1):])
+        else:
+            return str1
 
-        for l in range(min(len1, len2), 0, -1):
-            if isDivisor(l):
-                return str1[:l]
-        return ""
+sol = Solution()
+print(sol.gcdOfStrings("ABCABC", "ABC")) # ABC
+print(sol.gcdOfStrings("ABABAB", "ABAB")) # AB
+print(sol.gcdOfStrings("LEET", "CODE")) # ""
+print(sol.gcdOfStrings("ABCDEF", "ABC")) # ""
+print(sol.gcdOfStrings("ABC", "ABCDEF")) # ""
